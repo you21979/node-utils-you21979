68dc8b11c09d9db95a872a31106921847b63db17
you21979 <you2197901@gmail.com>
2013-08-13 16:24:59 +0900

    mod

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-var Class = exports.Class = require("./lib/class");
+exports.Class = require("./lib/class");
+exports.createLimitter = require("./lib/limitter");
+exports.StringUtil = require("./lib/string_util");
+exports.DateTime = require("./lib/date_time");
